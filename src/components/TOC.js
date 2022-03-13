e86e9ee2dc811cfd640e1bd0168c0e2c91f94e86
ParkIsComing <maroony55@gmail.com>
2022-03-13 21:21:06 +0900

    20220313

---
+++ src/components/TOC.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+
+class TOC extends Component {
+    render() {
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while(i < data.length){
+            lists.push(
+            <li key={data[i].id}>
+                <a
+                 href={"/content/"+data[i].id}
+                 data-id={data[i].id}
+                 onClick={function(e){
+                     e.preventDefault();
+                     this.props.onChangePage(e.target.dataset.id);//target은 이벤트가 일어난 tag를 가리키고 data-어쩌고는 .dataset.어쩌고로 접근 가능
+                 }.bind(this)}
+                 >{data[i].title}</a>
+            </li>);
+            i = i+1;
+        }
+
+        //onClick()을 아래처럼 적어도 o
+                //  onClick={function(id, e){
+                //     e.preventDefault();
+                //     this.props.onChangePage(e.target.dataset.id);//target은 이벤트가 일어난 tag를 가리키고 data-어쩌고는 .dataset.어쩌고로 접근 가능
+                // }.bind(this, data[i].id)}
+
+       return (
+        <nav>
+          <ul>
+            {lists}
+          </ul>
+        </nav>
+      );
+    }
+  }
+  
+
+  export default TOC;
